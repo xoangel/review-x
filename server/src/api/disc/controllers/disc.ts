4fab39d275afd81efdd7c78a516306f7d6f01c1c
xoangel <kennyrocker@yandex.ru>
2024-06-12 18:55:55 +0800

    конфигурация Strapi

---
+++ server/src/api/disc/controllers/disc.ts
@@ -0,0 +1,7 @@
+/**
+ * disc controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::disc.disc');
---
+++ server/src/api/disc/services/disc.ts
@@ -0,0 +1,7 @@
+/**
+ * disc service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::disc.disc');
